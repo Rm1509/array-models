c709a2a31494ded29e2ff64cce2673b657b13155
Rox Middleton <r.middleton@bristol.ac.uk>
2022-12-07 13:37:06 +0100

    script to run

---
+++ script_to_test_makeHelicoid.m
@@ -0,0 +1,7 @@
+% script to test makeHelicoid
+
+% this is one place that homefol has been sent 'C:\Users\Rox\OneDrive -
+% University of Bristol\Documents\lumerical beb\221201 multi tests\matfiles'
+
+eghandle = makeHelicoid(3,  1,      [2,2,3],  1, homefol, 100, 0.05  );
+
