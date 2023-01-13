50cf9b6c0194be38009826277f712719d3ddf287
Rox Middleton <r.middleton@bristol.ac.uk>
2023-01-13 10:11:08 +0100

    commented properly

--- multiparticle.m
+++ multiparticle.m
@@ -1,34 +1,53 @@
 function multiparticle()
+% 
+% -	Neaten up multiparticle so all of these things are inputs and outputs
+% -	Allow or disallow intersection
+% -	Control distribution in z – first attempt at this, but it doesn’t work very well
+% -	Add a grid of distribution in x-y which the deviation is away from
+% -	Control the angular orientation within a range
+% -	Save these and load them in Lumerical (NB. Radically different sizes!)
+% -	Control size and add these to Lumerical
+% -	Allow a range of sizes and a range of lengths
+% -	Twist the curls round the central corkscrew
+% -	Use different shapes
 
 
-
-% fff = figure; 
+% this is required if you want to save it,  but is not actually important
 homefol = 'C:\Users\Rox\OneDrive - University of Bristol\Documents\lumerical beb\multiparticleArrayDev\230112';
+
+%this calls the basic shape, for when the same shape is reproduced multiple
+%times
 eghandle = makeHelicoid(3,  1,   [2,2,3], homefol, 0,  100, 0.05);
 
-% p=patch('Vertices', eghandle.vertices, 'Faces', eghandle.faces);
-
-% p.FaceColor= 'cyan';
-% p.EdgeColor= 'cyan';
+%figure setup
 figure
 pcol = parula(9);
-for i = 1: 9
 
-p = patch('Vertices', eghandle.vertices+repmat([150,200*randn(1,2)], size(eghandle.vertices,1),1), 'Faces', eghandle.faces);
-p.FaceColor= pcol(i,:);
-p.EdgeColor= pcol(i,:);
-% rotate(p, [1,1,1].*randn(1,3), 45.*randn(1))
-rotate(p, [randn(1), randn(1), randn(1)], 45.*randn(1))
-hold on
+% to make a multiarray, each one is made in this loop
+for i = 1: 9
+    
+    %place a shape in space at a random location
+    p = patch('Vertices', eghandle.vertices+repmat([150,200*randn(1,2)], size(eghandle.vertices,1),1), 'Faces', eghandle.faces);
+    hold on
+
+    % indicate the colour of the shape
+    p.FaceColor= pcol(i,:);
+    p.EdgeColor= pcol(i,:);
+
+    %rotate the shape according to a distribution 
+    rotate(p, [randn(1), randn(1), randn(1)], 45.*randn(1))
+    
 end
-% aspect([1 1 1])
+
+% neaten up the plot
 looks(gcf)
 
 
 end
+
 function looks(ffig)
-ffig    
-view(3); 
+    ffig    
+    view(3); 
     axis tight
     camlight 
     lighting gouraud
