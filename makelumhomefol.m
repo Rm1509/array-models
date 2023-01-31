c2aaa7a17ed6dd4eb1a58872349c6a513aee8028
Rox Middleton <r.middleton@bristol.ac.uk>
2023-01-31 16:28:34 +0100

    gitlumhomefol makes a folder for today with the right subfolders

---
+++ makelumhomefol.m
@@ -0,0 +1,22 @@
+function makelumhomefol (homefol)
+
+cd(homefol)
+nom=DT4filename;
+nom=nom(4:9);
+newf=strcat(homefol, '/',nom);
+tstat= mkdir(newf);
+if tstat==1
+cd(newf)
+mkdir('matfiles')
+mkdir('lumfiles')
+mkdir('output')
+end
+
+
+
+
+
+
+
+
+end
